a7e806fbe3476f064057517953f951b135b77e0d
Jairo Valderrama <jairovg@gmail.com>
2021-10-06 20:07:10 -0500

    build: configure at-rule-empty-line-before rule
    Ignore blockless-after-same-name-blockless
    See: https://stylelint.io/user-guide/rules/list/at-rule-empty-line-before/#except-after-same-name-inside-block-blockless-after-same-name-blockless-blockless-after-blockless-first-nested

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,4 +2,12 @@ const STYLELINT_EXTENDS = require('webpack-lib').stylelint.STYLELINT_EXTENDS;
 
 module.exports = {
   extends: STYLELINT_EXTENDS,
+  rules: {
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['blockless-after-same-name-blockless'],
+      },
+    ],
+  },
 }
