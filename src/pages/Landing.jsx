514bf8ae1541fb863b66a56f268e5ff333b8f886
Jairo Valderrama <jairovg@gmail.com>
2021-10-17 15:07:02 -0500

    fix: get user info when user submit search form
    Sets the octocat user as a default user. To be used when no results

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,21 +1,55 @@
+import { onMounted, reactive } from 'vue';
 import Header from '~/components/Header';
 import DarkModeToggle from '~/components/DarkModeToggle';
 import SearchInput from '~/components/SearchInput';
+import getAUser from '~/api/github';
 import '~/assets/styles/global.scss';
 
-const App = () => (
-  <>
-    <Header>
-      <DarkModeToggle />
-    </Header>
-    <SearchInput />
+let DEFAULT_USER;
 
-    Joined
+const Landing = {
+  name: 'Landing',
+  setup() {
+    const user = reactive({
+      avatar: '',
+      name: '',
+      username: '',
+      joined: '',
+      bio: '',
+      repos: '',
+      followers: '',
+      following: '',
+      location: '',
+      blog: '',
+      twitter: '',
+      company: '',
+    });
 
-    Repos
-    Followers
-    Following
-  </>
-);
+    onMounted(async () => {
+      DEFAULT_USER = await getAUser('octocat');
+      Object.assign(user, DEFAULT_USER);
+    });
 
-export default App;
+    const searchUser = async (username) => {
+      if (username === 'octocat') {
+        Object.assign(user, DEFAULT_USER);
+      } else {
+        const result = await getAUser(username);
+        Object.assign(user, result);
+      }
+    };
+
+    return () => (
+      <>
+        <Header>
+          <DarkModeToggle />
+        </Header>
+        <SearchInput
+          onSubmit={ searchUser }
+        />
+      </>
+    );
+  },
+};
+
+export default Landing;
