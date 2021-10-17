d8cb714163e8a6c492de0018f3eaf8d51abca222
Jairo Valderrama <jairovg@gmail.com>
2021-10-17 13:52:04 -0500

    fix: handle search input submit action

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,28 +1,49 @@
+import { defineComponent, ref } from 'vue';
 import classnames from 'classnames';
 import s from '~/assets/styles/components/search-input.module.scss';
 
-const Search = (props) => (
-  <form
-    action=""
-    role="search"
-    class={ classnames(s.search) }
-  >
-    <input
-      id="search-user"
-      type="text"
-      name="username"
-      class={ classnames(s.search__input) }
-      placeholder={ props.placeholder || 'Search GitHub username…' }
-    />
-    <button
-      type="submit"
-      class={ classnames(s.search__button) }
-    >
-      Search
-    </button>
-  </form>
-);
+const Search = defineComponent({
+  name: 'SearchInput',
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Search GitHub username…',
+    },
+  },
+  emits: ['submit'],
+  setup(props, { emit }) {
+    const placeholder = ref(props.placeholder);
+    const username = ref('');
 
-Search.props = ['placeholder'];
+    const submithHandler = (e) => {
+      e.preventDefault();
+      emit('submit', username.value);
+    };
+
+    return () => (
+      <form
+        novalidate
+        role="search"
+        class={ classnames(s.search) }
+        onSubmit={ submithHandler }
+      >
+        <input
+          id="search-user"
+          type="text"
+          name="username"
+          class={ classnames(s.search__input) }
+          placeholder={ placeholder.value }
+          v-model={ username.value }
+        />
+        <button
+          type="submit"
+          class={ classnames(s.search__button) }
+        >
+          Search
+        </button>
+      </form>
+    );
+  },
+});
 
 export default Search;
