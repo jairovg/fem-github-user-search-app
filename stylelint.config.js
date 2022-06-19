55f29ba49df4a49daeb288c0b09b5fc497c5dc5c
Jairo Valderrama <jairovg@gmail.com>
2022-06-18 19:33:15 -0500

    build(stylelint): extends new stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,13 +1,3 @@
-const STYLELINT_EXTENDS = require('webpack-lib').stylelint.STYLELINT_EXTENDS;
-
 module.exports = {
-  extends: STYLELINT_EXTENDS,
-  rules: {
-    'at-rule-empty-line-before': [
-      'always',
-      {
-        except: ['blockless-after-same-name-blockless'],
-      },
-    ],
-  },
-}
+  extends: 'stylelint-config-standard-scss',
+};
